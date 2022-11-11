6f5408d0535fc0d4592fdb70b3866abb7aa1423c
Ryo Hirai <m1zuk1lac1e@gmail.com>
2022-11-12 02:37:08 +0900

    物体検出機能追加

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
-FROM python:3.9
+FROM python:3.10
 
 WORKDIR /app
 
 COPY requirements.txt .
+RUN pip install --upgrade pip
 RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
 
 COPY ./app /app
--- app/main.py
+++ app/main.py
@@ -1,11 +1,48 @@
-from typing import Union
-import io
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import StreamingResponse
+from PIL import Image, ImageDraw
+from matplotlib import pyplot as plt
+from io import BytesIO
+import torch
 
 app = FastAPI()
 
+"""
+ 物体検出
+"""
 @app.post("/detect/")
-async def detect(file: UploadFile = File(...)):
-    img = await file.read()
-    return StreamingResponse(io.BytesIO(img), media_type="image/png")
+async def detect(img: UploadFile = File(...)):
+    # 画像のロード
+    image = Image.open(img.file)
+
+    # 予測と描画
+    image = _object_detection(image)
+
+    response = BytesIO()
+    image.save(response, "JPEG")
+    response.seek(0)
+
+    return StreamingResponse(response, media_type="image/jpeg")
+    
+
+def _object_detection(image):
+    # 物体検出
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+    pred = model(image)
+
+    # 検出結果の描画
+    for detections in pred.xyxy:
+        # 色の一覧を作成
+        cmap = plt.cm.get_cmap("hsv", len(detections) + 1)
+
+        for i, detection in enumerate(detections):
+            class_name = str(model.model.names[int(detection[5])])
+            bbox = [int(x) for x in detection[:4].tolist()]
+            conf = float(detection[4])
+        
+            color = cmap(i, bytes=True)
+            draw = ImageDraw.Draw(image)
+            draw.rectangle(bbox, outline=color, width=3)
+            draw.text([bbox[0], bbox[1]-20], class_name, fill=color)
+
+    return image
